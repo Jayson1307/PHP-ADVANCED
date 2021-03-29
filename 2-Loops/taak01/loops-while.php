cbc93474a4629e624c5e210050a35f2ac3623a9d
Jayson1307 <2125083@talnet.nl>
2021-03-29 11:58:05 +0200

    while-loop 1

---
+++ 2-Loops/taak01/loops-while.php
@@ -0,0 +1,17 @@
+<html>
+<head></head>
+<body>
+
+<?php
+    $leeftijd = 0;
+    while ($leeftijd <= 18) {
+        echo "<ul>ik ben $leeftijd jaar oud, dus ik mag nog niet stemmen</ul>";
+        $leeftijd++;
+       
+    } 
+    echo "<ul>ik ben $leeftijd jaar oud, dus ik heb stemrecht</ul>";
+
+?>
+
+</body>
+</html>
