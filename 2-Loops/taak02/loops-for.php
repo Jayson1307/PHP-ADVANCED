b25e382e79b835c3d376499172c7d6f1184fde6d
Jayson1307 <2125083@talnet.nl>
2021-03-29 12:12:31 +0200

    loops-for opdracht af

---
+++ 2-Loops/taak02/loops-for.php
@@ -0,0 +1,7 @@
+<?php
+    $x;
+    for ($x=0; $x <18 ; $x++) { 
+        echo "<ul>Ik ben $x jaar, dus ik mag nog niet stemmen</ul>";
+    }
+    echo "<ul> ik ben $x jaar oud, dus ik heb stemrecht </ul>"
+?>
