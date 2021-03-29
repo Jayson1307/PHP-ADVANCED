6548b6ff6c4d69a53b694dac387ca561742d095d
Jayson1307 <2125083@talnet.nl>
2021-03-29 10:51:10 +0200

    opdracht 2 af

--- 1-Arrays/taak02/index.php
+++ 1-Arrays/taak02/index.php
@@ -1,4 +1,4 @@
 <?php
-$persoon = array("voornaam"=>'Jayson', "achternaam"=>van Olffen, "leeftijd"=>16, "opleiding"=>'Software Developer');
-echo 
+$persoon = array("voornaam"=>'Jayson', "achternaam"=>'van Olffen', "leeftijd"=>16, "opleiding"=>'Software Developer');
+echo "ik ben " . $persoon['voornaam'] . " " . $persoon['achternaam'] . " ik ben " . $persoon['leeftijd'] . " en ik doe de opleiding " . $persoon['opleiding'];
 ?> 
