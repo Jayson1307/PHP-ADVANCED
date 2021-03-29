ce864c9c5852ea80f982a4b700bfee76f9e25e73
Jayson1307 <2125083@talnet.nl>
2021-03-29 10:20:22 +0200

    opdracht 1

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,4 @@
+<?php
+$piet = array("voornaam"=>'Muhammad', "achternaam"=>184, "leeftijd"=>16, "klas"=>'0D');
+echo "voornaam = ". $piet ["voornaam"], " achternaam = " . $piet ["achternaam"], " leeftijd = " . $piet["leeftijd"], " en zit in klas= " . $piet["klas"];
+?>\
