1eb3ba632be17aea9d15a30d984f95c78d507c9f
Jayson1307 <2125083@talnet.nl>
2021-04-01 13:35:31 +0200

    foreach 2 klaar

--- 2-Loops/taak04/loops-foreach-deel 2.php
+++ 2-Loops/taak04/loops-foreach-deel 2.php
@@ -1,16 +1,21 @@
+<!DOCTYPE html>
 <html>
 <body>
-
-<label for="fname">First name:</label>
-<input type="text" id="fname" name="fname">
-
+ 
 <?php
-        $klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
-        foreach ($klassen as $key => $value) {
-            
-        }
+$klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+ 
 ?>
-
+<form method="post">
+<select name="Klasnaam">
+    <option value="">--Please choose an option--</option>
+    <?php
+    foreach($klassen as $klas) { ?>
+      <option value="<?php echo $klas; ?>">klas: <?php echo $klas; ?> </option>
+  <?php
+    } ?>
+</select>
+<input type="submit" value="versturen">
+</form>
 </body>
 </html>
-    
