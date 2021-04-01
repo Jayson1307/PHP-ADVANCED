7be0f217c5f323309d8fb36140dbfe57dd5e6d42
Jayson1307 <2125083@talnet.nl>
2021-04-01 13:34:28 +0200

    beetje klooien foreach 2

---
+++ 2-Loops/taak04/loops-foreach-deel 2.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<label for="fname">First name:</label>
+<input type="text" id="fname" name="fname">
+
+<?php
+        $klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+        foreach ($klassen as $key => $value) {
+            
+        }
+?>
+
+</body>
+</html>
+    
