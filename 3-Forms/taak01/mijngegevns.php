ac5c83d80c1489951be47803e96b4b319d70695b
Jayson1307 <2125083@talnet.nl>
2021-04-06 12:06:08 +0200

    forms opdracht 1

---
+++ 3-Forms/taak01/mijngegevns.php
@@ -0,0 +1,10 @@
+<html>
+<head></head>
+<body>
+    <?php
+        echo "naam is " . $_POST["voornaam"] . " " . $_POST["achternaam"] . ", " . "zit in klas: " . $_POST["klas"] . ", is ". $_POST["leeftijd"] . " jaar oud, woont in de " . 
+        $_POST['adres']  . ' in ' . $_POST['plaatsnaam'] . ' en luisterd graag naar ' . $_POST['muziek'] ;
+        
+    ?>
+</body>
+</html>
