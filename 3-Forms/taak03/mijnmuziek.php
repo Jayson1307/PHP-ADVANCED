17d4d7cd41e1ffe8569c2eac2c28d9f1779e2b75
Jayson1307 <2125083@talnet.nl>
2021-04-08 13:41:49 +0200

    opdracht 3 forms

---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>taak03-Forms</title>
+</head>
+<body>
+    <?php
+    echo "<ul> ariest favoriete nummer: " . $_GET["artiest"] . "</ul> <br>" ;
+   echo "<ul> favoriete nummer: " . $_GET["titel"] . "</ul> <br>";
+   echo "<ul> jaar van uitkomst: " . $_GET["jvu"] . "</ul> <br>";
+   echo "<ul> muzieksoort: " . $_GET["genre"] . "</ul> <br>";
+     
+    ?>
+</body>
+</html>
