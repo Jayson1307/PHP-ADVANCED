0430b359e80558e11849aa20e7f8b52954cc124f
Jayson1307 <2125083@talnet.nl>
2021-04-15 14:21:09 +0200

    savepoint 2

--- opdracht/advanced-1.php
+++ opdracht/advanced-1.php
@@ -7,6 +7,17 @@
     <title>Document</title>
 </head>
 <body>
+    <h3>letterkleur:</h3>
+    <form action="radio">
+        <select name="tekstkleur" id="tekstkleur">
+        <?php $kleuren = array("red ", "blue ", "green ", "black ", "brown");
+    foreach ($kleuren as $tekstkleur) {
+        echo "<option value=$tekstkleur>$tekstkleur>";
+    }?>
+    <br>
+        </select>
+        <input type="text" name="border">
+    </form>
     
 </body>
 </html>
